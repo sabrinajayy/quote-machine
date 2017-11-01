e62d0fe76088398768a80b2f873e8d90e6d0b002
Sabrina John <sabrina.john1990@gmail.com>
2017-11-01 16:27:47 -0400

    used quote site api to generate random quote

--- js/app.js
+++ js/app.js
@@ -1,1 +1,26 @@
+$(document).ready(function () {
+    // $.ajax({
+    //     url: "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand"
+    // }).then(function(data) {
+    //    $('.quote-text').append(data[0].content);
+    //    $('.quote-author').append(data[0].title);
+    // });
+
+
+
+  $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(json) {
+    var html = "";
+    html += "<div class = 'quote-text text-center'>";
+    html += json[0].content;
+    html += "</div>";
+    html += "<div class = 'quote-author text-right'>";
+    html += json[0].title;
+    html += "</div>";
+
+    $(".quote-content").html(html);
+  });
+
+
+
+});
 
