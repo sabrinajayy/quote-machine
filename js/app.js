9c26e820ac2848a3401c3681d974ce7c0a9bf5d8
Sabrina John <sabrina.john1990@gmail.com>
2017-11-01 17:42:43 -0400

    added changeBackground function that changes bkgrd color on click of button

--- js/app.js
+++ js/app.js
@@ -1,29 +1,41 @@
+var backgrounds = ["#a426ff", "#54e568", "#fced44", "#ff4747", "#65c6ed"];
+
 $(document).ready(function () {
+  getQuote();
 
   function getQuote() {
-      $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=40&callback=", function(json) {
+    $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=40&callback=", function(json) {
       var html = "";
       var randomIndex = Math.floor(Math.random() * json.length + 1);
       html += "<div class = 'quote-text text-center'>";
       html += json[randomIndex].content;
       html += "</div>";
       html += "<div class = 'quote-author text-right'>";
-      html += json[randomIndex].title;
+      html += "- " + json[randomIndex].title;
       html += "</div>";
 
-      $(".quote-content").html(html);
-    });
+      // $(".quote-content").html(html);
+
+      $('.quote-content').fadeTo('slow', 0.3, function() {
+          $(this).html(html);
+        }).fadeTo('slow', 1);
+      });
   }
 
-  getQuote();
+  function changeBackground() {
+    var bkgrdColor = backgrounds[Math.floor(Math.random() * backgrounds.length)];
+    $('.page-wrapper').fadeTo('slow', 0.3, function() {
+        $(this).css('background-color', bkgrdColor);
+    }).fadeTo('slow', 1);
+  }
 
-  $(".quote-actions a").click(function (e){
+  $(".quote-btn").click(function (e){
+    changeBackground();
     getQuote();
   });
 
-
-
-
-
 });
+
+
+
 
