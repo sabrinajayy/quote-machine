e90f0245ea6578e377706d5072dea6a1b970d59a
Sabrina John <sabrina.john1990@gmail.com>
2017-11-01 22:17:06 -0400

    changed http to https

--- js/app.js
+++ js/app.js
@@ -3,39 +3,10 @@ var backgrounds = ["#a426ff", "#54e568", "#fced44", "#ff4747", "#65c6ed"];
 $(document).ready(function () {
   getQuote();
 
-  // function getQuote() {
-  //   $.getJSON("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=40&callback=", function(json) {
-  //     var html = "";
-  //     var randomIndex = Math.floor(Math.random() * json.length + 1);
-  //     var currentQuote = json[randomIndex].content.slice(3, -4);
-  //     var currentAuthor = "- " + json[randomIndex].title
-  //     html += "<div class = 'quote-text text-center'>";
-  //     html += json[randomIndex].content;
-  //     html += "</div>";
-  //     html += "<div class = 'quote-author text-right'>";
-  //     html += currentAuthor;
-  //     html += "</div>";
-
-  //     $('.quote-content').fadeTo('slow', 0.3, function() {
-  //         $(this).html(html);
-  //     }).fadeTo('slow', 1);
-
-  //     //encode quote and author text
-  //     var encodedQuote = encodeURIComponent(currentQuote);
-  //     var encodedAuthor = encodeURIComponent(currentAuthor);
-
-  //     $(".tweet-btn").fadeTo('slow', 0.3, function() {
-  //         $(this).html("<a href='https://twitter.com/intent/tweet?text=" + encodedQuote + encodedAuthor + "' target='_blank'><i class='fa fa-twitter-square fa-4x' aria-hidden='true'></i></a>");
-  //     }).fadeTo('slow', 1);
-
-  //     }); //ends getJSON function
-  // } //end getQuote function
-
-
    function getQuote() {
         $.ajax({
         type: 'GET',
-        url: 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=40',
+        url: 'https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=40',
         data: { get_param: 'value' },
         dataType: 'json',
         crossDomain: true,
@@ -65,13 +36,6 @@ $(document).ready(function () {
         }
     });
    }
-
-
-
-
-
-
-
 
 
   function changeBackground() {
